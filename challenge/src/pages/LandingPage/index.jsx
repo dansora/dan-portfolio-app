fe04ffed49500fde4292340b59825299d8e41437
dansora <dannysora@gmail.com>
2024-02-16 19:14:10 +0000

    redone the tree structure

---
+++ challenge/src/pages/LandingPage/index.jsx
@@ -0,0 +1,14 @@
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>
+                Welcome
+            </h1>
+            <div>
+                Welcome to my Portfolio Page!
+            </div>
+        </div>
+    );
+};
+
+export LandingPage;
