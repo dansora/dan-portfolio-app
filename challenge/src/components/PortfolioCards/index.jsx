b94c5e44a994ecf1a2774069ad3d4091506b16dc
dansora <dannysora@gmail.com>
2024-02-19 20:12:41 +0000

    added pages and more code on pages

---
+++ challenge/src/components/PortfolioCards/index.jsx
@@ -0,0 +1,17 @@
+const PortfolioCards = (props) => {
+// check props object
+console.log(props);
+
+// use props
+return(
+    <div className="card">
+        <div className="card-body">
+            <h5 className="card-title">{props.restName}</h5>
+            <h6 className="card-subtitle mb-2 text-body-secondary">{props.restPhone}</h6>
+            <p className="card-text">{props.restAddress}</p>
+        </div>
+    </div>
+  );
+};
+
+export default PortfolioCards;
--- challenge/src/pages/PotfolioGallery/index.jsx
+++ challenge/src/pages/PotfolioGallery/index.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import PortfolioGallery from "../../components/PortfolioCards"
 
 const PortfolioGallery = () => {
     const [responseData, setResponseData] = useState({
@@ -9,11 +10,48 @@ const PortfolioGallery = () => {
 
     const getPotfolioData = () => {
         axios
-         .get ()
+         .get (`https://random-data-api.com/api/restaurant/random_restaurant?size=6`)
          .then((res) => {
             console.log(res.status === 200);
-            setResponseData(res.data);
+            setResponseData({
+                isDataAvailable: res.status === 200,
+                results: res.data,
+            });
         });
     };
+
+
+return(
+    <div>
+        <div>
+            Search For Projects
+        </div>
+        {/* conditionally rendering the search button */}
+        {!responseData.isDataAvailable && (
+            <button onClick={getPortfolioData}>Search</button>
+        )}
+
+        {/* conditionally rendering the Portfolio Cards */}
+        {responseData.isDataAvailable && (
+            <div>
+                {responseData.results.map((restaurant) => {
+                    return (
+                        <>
+                        <PortfolioCard
+                        key={restaurant.id}
+                        restName={restaurant.name}
+                        restPhone={restaurant.phone_number}
+                        restAddress={restaurant.address}
+                        />
+                        </>
+                    );
+                })}
+            </div>
+        )}    
+
+    </div>
+);
 };
+
+export default PortfolioGallery;
 
