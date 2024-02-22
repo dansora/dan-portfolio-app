5c659f9b3004c44a979ff7e5562cfa0194b96665
dansora <dannysora@gmail.com>
2024-02-22 21:48:46 +0000

    new commit

--- reactpractice/src/src/components/NavigationBar/index.jsx
+++ reactpractice/src/src/components/NavigationBar/index.jsx
@@ -12,8 +12,7 @@ const NavigationBar = () => {
           data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+          aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
